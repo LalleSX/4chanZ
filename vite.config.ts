d0c96bcb28da104e87008286a703fff3f4c3dc0e
Lalle <29478339+LalleSX@users.noreply.github.com>
2023-05-22 03:16:39 +0200

    remove unnessesary configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 /* eslint-disable indent */
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import webExtension from "@samrum/vite-plugin-web-extension"
 import path from "path"
 import { getManifest } from "./src/manifest"
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "")
+export default defineConfig(() => {
   return {
     plugins: [
       react(),
